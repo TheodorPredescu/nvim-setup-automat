95ad939f14676b5e76b503dae9e9223b13a63812
TheodorPredescu <130825375+TheodorPredescu@users.noreply.github.com>
2024-01-13 20:42:41 +0200

    commit correct

--- test.cpp
+++ test.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 
 int main() {
-    printf("merge");
-    printf("hai sa mancam!");
-    printf("merge");
-    printf("hai sa mancam!");
-    printf("merge");
-    printf("hai sa mancam!");
-    printf("merge");
-    printf("hai sa mancam!");
+    
     return 0;
 }
