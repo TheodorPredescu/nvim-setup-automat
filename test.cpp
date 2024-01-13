ff89d1b346021969632e2f71f10b9780f8635485
TheodorPredescu <130825375+TheodorPredescu@users.noreply.github.com>
2024-01-13 20:42:10 +0200

    commit gresit

--- test.cpp
+++ test.cpp
@@ -3,5 +3,11 @@
 int main() {
     printf("merge");
     printf("hai sa mancam!");
+    printf("merge");
+    printf("hai sa mancam!");
+    printf("merge");
+    printf("hai sa mancam!");
+    printf("merge");
+    printf("hai sa mancam!");
     return 0;
 }
