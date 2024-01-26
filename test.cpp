9728d09725ad23cce1d242aa8b678d2718023710
TheodorPredescu <130825375+TheodorPredescu@users.noreply.github.com>
2024-01-26 13:39:13 +0200

    close execc

--- test.cpp
+++ test.cpp
@@ -1,6 +1,33 @@
+#include <windows.h>
+#include <tlhelp32.h>
 #include <iostream>
 
+void ListProcesses() {
+    const char* processName = "WhatsApp.exe";
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error creating process snapshot" << std::endl;
+        return;
+    }
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    if (Process32First(hSnapshot, &pe32)) {
+        do {
+            if (_stricmp(pe32.szExeFile, processName) == 0) {
+                    HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pe32.th32ProcessID);
+                    if (hProcess != NULL) {
+                        TerminateProcess(hProcess, 0);
+                        CloseHandle(hProcess);
+                        return;
+                    }
+            }
+        } while (Process32Next(hSnapshot, &pe32));
+    }
+    CloseHandle(hSnapshot);
+}
+
 int main() {
-    
+    ListProcesses();
     return 0;
 }
