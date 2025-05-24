6810f649fd2020d40a1e8621cacabe167d33205c
TheodorPredescu <predescutheodor@gmail.com>
2025-05-24 19:42:07 +0300

    added first install code

--- bash.sh
+++ bash.sh
@@ -4,11 +4,34 @@ source packages.conf
 
 echo "Works fine"
 
-for package in ${packages[@]}; do
-    if dpkg -s ${package} >/dev/null 2>&1; then
-        echo "${package} is installed."
-    else
-        echo "${package} is not installed."
-        echo "Installing..."
-    fi
-done
+sudo -v
+# checking if lazyvim is installed
+if dpkg -s neovim >/dev/null 2>&1; then
+    echo "nvim is configurated"
+else
+    echo -e "nvim is not configurated\n     Installing..."
+    sudo apt update
+    sudo apt install -y neovim
+fi
+
+if [[ -d ${HOME}/.config ]]; then
+    echo "Making the .config file"
+    mkdir "${HOME}/.config"
+fi
+
+if [[ -d "/home/${USER}/.config/nvim" ]]; then
+    echo "nvim alread configurated. The process will end here"
+    exit 0
+else
+    echo "nvim not configurated"
+    git clone https://github.com/TheodorPredescu/nvim-lazyvim_config "${HOME}/.config/nvim"
+fi
+
+# for package in "${packages[@]}"; do
+#     if dpkg -s "${package}" >/dev/null 2>&1; then
+#         echo "${package} is installed."
+#     else
+#         echo "${package} is not installed."
+#         echo "Installing..."
+#     fi
+# done
